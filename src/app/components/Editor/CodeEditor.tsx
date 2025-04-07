a82313418367ca930faeefadba7298e979ac6289
dxt <prof.mohdali@outlook.com>
2025-04-07 19:49:39 +0530

    go again

--- src/app/components/Editor/CodeEditor.tsx
+++ src/app/components/Editor/CodeEditor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import dynamic from "next/dynamic";
 
 import { useLanguage } from "@/app/middleware/LanguageContext";
